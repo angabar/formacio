3ad1cd6582d9e0d77088245403a02eafbb17433f
encece <enriccc15@gmail.com>
2022-02-23 19:00:12 +0100

    Added lessons

--- python/06/06.py
+++ python/06/06.py
@@ -1,3 +1,6 @@
+from random import shuffle
+
+
 # FUNCTIONS
 def say_hello(name="Default"):
     print(f"jajajaj {name}")
@@ -63,3 +66,79 @@ print(price_check(stock_prices))
 # queramos, pero cuidado con acceder a elementos que no existen
 name_max, price_max = price_check(stock_prices)
 print(name_max)
+
+example_list = [1, 2, 3, 4, 5, 6, 7]
+
+
+# shuffle no devuelve nada, pero podemos hacer una funcion para devolver una
+# alteracion de la lista que entre
+def shuffle_list(my_list):
+    shuffle(my_list)
+    return my_list
+
+
+result = shuffle_list(example_list)
+
+my_game_list = [" ", "0", " "]
+
+
+def player_guess():
+    guess = ""
+    while guess not in ["0", "1", "2"]:
+        guess = input("pick a number 0, 1 or 2\n")
+
+    return int(guess)
+
+
+def check_guess(my_list, user_guess):
+    if my_list[user_guess] == "0":
+        print("Correct")
+    else:
+        print("Wrong guess")
+        print(my_list)
+
+
+# Creamos la lista y la alteramos, creamos la eleccion del usuario y por ultimo
+# lo combinamos todo
+shuffled_balls = shuffle_list(my_game_list)
+player_guess = player_guess()
+
+check_guess(shuffled_balls, player_guess)
+
+# args and kwargs (arguments and keyword arguments)
+
+
+def myfunc(num1, num2):
+    return (num1 + num2) * 0.05
+
+
+print(myfunc(40, 60))
+
+
+# Con args tenemos acceso a todos los parametros de una funcion metidos en un
+# tupla
+def myargsfunc(*args):
+    print(args)
+    return sum(args) * 0.05
+
+
+print(myargsfunc(40, 60))
+
+
+def mykwargsfunc(**kwargs):
+    print(kwargs)
+
+
+mykwargsfunc(test="placa")
+
+
+# En python podemos usar dos tipos de argumentos, como en Dart, los
+# posicionales, los tipicos, y los asociados a une key, los ultimos se definen
+# con el mismo nombre por el cual sera utilizado en la variable
+def testfunc(myvalue):
+    print(myvalue)
+
+
+# Cuidado, acceder a una propiedad asociativa tenemos que asegurarnos de que la
+# key existe en la funcion o tendremos un error
+testfunc(myvalue="test")
