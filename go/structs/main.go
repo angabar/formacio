441f7706b41ed4404734eb3f6530fb962555a1d9
Enric <enriccc15@gmail.com>
2022-06-01 19:58:11 +0200

    More lessons

--- go/structs/main.go
+++ go/structs/main.go
@@ -2,19 +2,60 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+/* type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+} */
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 	/* alex := person{firstName: "Alex", lastName: "Anderson"}
 	fmt.Println(alex) */
 
-	var alex person
+	/* var alex person
 
 	alex.firstName = "test"
 
 	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", alex) */
+
+	/* jim := person{
+		firstName: "Jim",
+		lastName:  "Jaja",
+		contact: contactInfo{
+			email:   "test@test.com",
+			zipCode: 123123,
+		},
+	} */
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Jaja",
+		contactInfo: contactInfo{
+			email:   "test@test.com",
+			zipCode: 123123,
+		},
+	}
+
+	jim.updateName("jajajaa")
+	jim.print()
+}
+
+func (singlePerson person) updateName(newFirstName string) {
+	singlePerson.firstName = newFirstName
+}
+
+func (singlePerson person) print() {
+	fmt.Printf("%+v", singlePerson)
 }
