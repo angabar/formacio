8031ec9d374c50285d5ce9e89f0e3f4cbbc49218
encece <enriccc15@gmail.com>
2022-06-01 09:06:16 +0200

    More lessons

---
+++ go/structs/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	/* alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex) */
+
+	var alex person
+
+	alex.firstName = "test"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+}
