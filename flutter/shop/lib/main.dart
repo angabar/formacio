4db25bc3533d6bb108666ebaefa80fb93611426d
Enric <enriccc15@gmail.com>
2022-06-27 20:04:53 +0200

    More code

--- flutter/shop/lib/main.dart
+++ flutter/shop/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/screens/product_detail_screen.dart';
 import 'package:shop/screens/products_overview_screen.dart';
 
@@ -11,29 +13,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        primarySwatch: Colors.purple,
-        colorScheme: const ColorScheme(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
-          surface: Colors.purpleAccent,
-          background: Colors.white,
-          error: Colors.red,
-          onPrimary: Colors.white,
-          onSecondary: Colors.white,
-          onSurface: Colors.white,
-          onBackground: Colors.white,
-          onError: Colors.redAccent,
-          brightness: Brightness.light,
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          primarySwatch: Colors.purple,
+          colorScheme: const ColorScheme(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+            surface: Colors.purpleAccent,
+            background: Colors.white,
+            error: Colors.red,
+            onPrimary: Colors.white,
+            onSecondary: Colors.white,
+            onSurface: Colors.white,
+            onBackground: Colors.white,
+            onError: Colors.redAccent,
+            brightness: Brightness.light,
+          ),
         ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) =>
+              const ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => const ProductDetailScreen(),
-      },
     );
   }
 }
