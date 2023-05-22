ceb667b1f6a777e0f83ecb81966bc0eeb1db1258
Enric <enriccc15@gmail.com>
2023-05-22 21:36:13 +0200

    More code

--- flutter/dice_roll/lib/gradient_container.dart
+++ flutter/dice_roll/lib/gradient_container.dart
@@ -1,25 +1,37 @@
-import 'package:dice_roll/main_title.dart';
 import 'package:flutter/material.dart';
 
+import 'package:dice_roll/dice_roller.dart';
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({
+    super.key,
+    required this.firstColor,
+    required this.secondColor,
+  });
+
+  const GradientContainer.purple({super.key})
+      : firstColor = Colors.purple,
+        secondColor = Colors.deepPurple;
+
+  final Color firstColor;
+  final Color secondColor;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
           colors: [
-            Colors.deepPurple,
-            Colors.purple,
+            firstColor,
+            secondColor,
           ],
         ),
       ),
       child: const Center(
-        child: MainTitle(
-          text: "Hello world",
+        child: Center(
+          child: DiceRoller(),
         ),
       ),
     );
