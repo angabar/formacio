aa629ec654d816fdcc078189a9c3e78ea072f45a
Enric <enriccc15@gmail.com>
2023-05-23 19:41:13 +0200

    More code

---
+++ flutter/quiz_app/lib/start_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+            color: const Color.fromRGBO(255, 255, 255, 0.8),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 30.0,
+            ),
+            child: const Text(
+              'Flutter quiz app',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(
+              Icons.arrow_right_alt,
+            ),
+            label: const Text(
+              'Enter the quiz',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
