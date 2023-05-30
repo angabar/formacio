db17d59aa6f278ca80d3f8491464f4b7828c21dd
Enric <enriccc15@gmail.com>
2023-05-30 18:49:22 +0200

    More code

--- flutter/expense_tracker/lib/widgets/expenses.dart
+++ flutter/expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -34,11 +35,13 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('Charts'),
-          Text('List'),
+          const Text('Charts'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
---
+++ flutter/expense_tracker/lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,43 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem({
+    super.key,
+    required this.expense,
+  });
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('${expense.amount.toStringAsFixed(2)} €'),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        child: Icon(categoryIcons[expense.category]),
+                      ),
+                      Text(expense.formattedDate),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
