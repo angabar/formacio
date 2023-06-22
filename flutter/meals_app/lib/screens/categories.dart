81f3343b1f6a06485d86fd7e67795fb04aabd065
encece <enriccc15@gmail.com>
2023-06-22 20:00:46 +0200

    More code

--- flutter/meals_app/lib/screens/categories.dart
+++ flutter/meals_app/lib/screens/categories.dart
@@ -136,9 +136,15 @@ class _CategoriesScreenState extends ConsumerState<CategoriesScreen>
               ),
           ],
         ),
-        builder: (BuildContext context, Widget? child) => Padding(
-          padding: EdgeInsets.only(
-            top: 100 - _animationController.value * 100,
+        builder: (BuildContext context, Widget? child) => SlideTransition(
+          position: Tween(
+            begin: const Offset(0, 0.3),
+            end: const Offset(0, 0),
+          ).animate(
+            CurvedAnimation(
+              parent: _animationController,
+              curve: Curves.easeInOut,
+            ),
           ),
           child: child,
         ),
