94cc742110163a09caf02c5247e237e98565e0c0
encece <enriccc15@gmail.com>
2026-08-16 09:25:30 +0200

    more code

--- web-components/shadow-dom/shadow-test.js
+++ web-components/shadow-dom/shadow-test.js
@@ -1,5 +1,11 @@
 class ShadowTest extends HTMLElement {
-    connectedCallback() {
+    // Cuando usamos shadowDOM podemos usar el constructor en lugar de
+    // connectedCallback para inicializar el HTML y los listenners, puesto que
+    // el DOM, al ser un fragment está disponible de manera inmediata, no es
+    // necesario esperar a que esté anclado al DOM principal
+    constructor() {
+        super();
+
         this.attachShadow({ mode: "open" });
 
         this.shadowRoot.innerHTML = `<div class="test">hola shadow</div>`;
