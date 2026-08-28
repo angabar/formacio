c0dea5919744e92f099e1e30f78952b33931406d
encece DAILY <enric.cebolla@idrica.com>
2026-08-28 18:08:56 +0200

    more code

--- web-components/super-colorpicker/components/coordpicker/template.js
+++ web-components/super-colorpicker/components/coordpicker/template.js
@@ -19,38 +19,40 @@ export default {
     },
     css(props) {
         return `
-            :host {
-                display: inline-block;
-                position: relative;
-            }
+            <style>
+                :host {
+                    display: inline-block;
+                    position: relative;
+                }
 
-            #bg-overlay-a {
-                width: 100%;
-                height: 100%;
-                border-radius: 3px;
-                position: absolute;
-                background: linear-gradient(to right, #fff 0%, rgba(255, 255, 255, 0) 100%);
-            }
+                #bg-overlay-a {
+                    width: 100%;
+                    height: 100%;
+                    border-radius: 3px;
+                    position: absolute;
+                    background: linear-gradient(to right, #fff 0%, rgba(255, 255, 255, 0) 100%);
+                }
 
-            #bg-overlay-b {
-                width: 100%;
-                height: 100%;
-                border-radius: 3px;
-                position: absolute;
-                background: linear-gradient(to bottom, transparent 0%, #000 100%);
-            }
+                #bg-overlay-b {
+                    width: 100%;
+                    height: 100%;
+                    border-radius: 3px;
+                    position: absolute;
+                    background: linear-gradient(to bottom, transparent 0%, #000 100%);
+                }
 
-            #thumb {
-                width: 5px;
-                height: 5px;
-                position: absolute;
-                border-style: solid;
-                border-width: 3px;
-                border-color: white;
-                border-radius: 6px;
-                pointer-events: none;
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-            }
+                #thumb {
+                    width: 5px;
+                    height: 5px;
+                    position: absolute;
+                    border-style: solid;
+                    border-width: 3px;
+                    border-color: white;
+                    border-radius: 6px;
+                    pointer-events: none;
+                    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                }
+            </style>
         `;
     },
 };
